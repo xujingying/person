a516ee9250d6f31e11efe9aaa76e9728b2e90a4f
blinkfox <chenjiayin1990@163.com>
2017-04-02 22:56:30 +0800

    更新pom.xml文件中的plugins和新增HelloWorld输出banner内容的功能

--- src/main/java/com/xjy/person/test/HelloWord.java
+++ src/main/java/com/xjy/person/test/HelloWord.java
@@ -1,9 +1,47 @@
 package com.xjy.person.test;
 
+import java.io.*;
+import java.net.URL;
+
 public class HelloWord {
-	
+
+    /**
+     * 私有构造方法.
+     */
+    private HelloWord() {
+        super();
+    }
+
 	public static void main(String[] args) {
-		System.out.println("Hello Word");
+        URL banner = HelloWord.class.getResource("/banner.txt");
+        if (banner == null) {
+            System.out.println("未获取到banner.txt文件!");
+            return;
+        }
+
+		File file = new File(banner.getPath());
+		System.out.println(txt2String(file));
 	}
-	
+
+	/**
+	 * 读取txt文件的内容
+	 * @param file 想要读取的文件对象
+	 * @return 返回文件内容
+	 */
+	public static String txt2String(File file){
+		StringBuilder result = new StringBuilder();
+		//构造一个BufferedReader类来读取文件
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+			String s = null;
+			while ((s = br.readLine()) != null) {//使用readLine方法，一次读一行
+				result.append(System.lineSeparator() + s);
+			}
+
+			br.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result.toString();
+	}
+
 }
